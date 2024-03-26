f6ad0d48b6372f1a2961a52104b8e3d617475cc7
Phuc_Bach_02 <vtpyn25664888@gmail.com>
2024-03-26 14:38:28 +0700

    tÃest v1.0

---
+++ src/main/java/car/controller/Main.java
@@ -0,0 +1,10 @@
+package car.controller;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=a+5;
+        System.out.println("b = "+b);
+        System.out.println("Hello world!");
+    }
+}
