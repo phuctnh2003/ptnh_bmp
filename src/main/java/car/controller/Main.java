e7258d39ebc8825e0e475a4b5382aeb44c479c43
Phuc_Bach_02 <vtpyn25664888@gmail.com>
2024-03-26 16:28:34 +0700

    sua

--- src/main/java/car/controller/Main.java
+++ src/main/java/car/controller/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int a=10;
         int b=a+5;
+        int c=444;
         System.out.println("b = "+b);
         System.out.println("Hello world!");
     }
